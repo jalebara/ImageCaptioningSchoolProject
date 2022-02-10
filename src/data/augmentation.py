5c6c30b240ae6d5fadd67497daab26fa3e67f1c8
Joshua Kalyanapu <jkalyan@ncsu.edu>
2022-02-10 23:36:53 +0000

    .gitignore'd emacs autosave and recovery files. created samples dir. deleted data.py. augmentation.py downsamples images to 224x224
    * samples/0_224x224_downsample.jpg:

--- src/data/augmentation.py
+++ src/data/augmentation.py
@@ -5,10 +5,18 @@ following should be implemented
  - Child Class that performs data augmentation on Flicker30K dataset
 """
 
-from data.data import FlickerDataset
+#from data.data import FlickrDataset
 
-class AugmentedFlickerDataset(FlickerDataset):
-    def __init__(self) -> None:
-        super().__init__()
-        raise NotImplementedError
+from torchvision.datasets import Flickr30k
+import torchvision.transforms as transforms
+from PIL import Image
 
+class AugmentedFlickrDataset(Flickr30k):
+    def __init__(self, root="../../flickr30k", annotations_file="../../flickr30k/results_20130124.token") -> None:
+        super().__init__(root, annotations_file, transform=transforms.Resize([224, 224]))
+        image = self[0][0]
+        print(image)
+        image.save("./0_224x224_downsample.jpg")
+
+if __name__=="__main__":
+    afd = AugmentedFlickrDataset()
