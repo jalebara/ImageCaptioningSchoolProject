ca635e48db448f63f58f12ce2792289f679efdbb
Joshua Kalyanapu <jkalyan@ncsu.edu>
2022-02-12 05:27:12 +0000

    Forgot to make transforms use params in __init__ Update __init__ comments, delete superfluous comments.

--- src/data/augmentation.py
+++ src/data/augmentation.py
@@ -14,17 +14,17 @@ class AugmentedFlickrDataset(Flickr30k):
                  root="../../flickr30k",
                  # Location of annotations file
                  annotations_file="../../flickr30k/results_20130124.token",
-                 # Tuple that is resize height and width
+                 # Tuple that is resize height and width (default 224x224)
                  resize=[224, 224],
-                 # Tuple that is range of degrees the image should be randomly rotated
+                 # Tuple that is range of degrees the image should be randomly rotated (default 0-360)
                  degrees=[0, 360],
-                 # Tuple that is range for random translation of image
+                 # Tuple that is range for random translation of image (default translate at most 1/5 in x and y direction)
                  translate=[0.2, 0.2],
-                 # Mean values of the Gaussian blur kernel size
+                 # Mean values of the Gaussian blur kernel size (default x=5, y=5)
                  blur_kernel_mean=(5, 5),
-                 # Range of std deviation of the Gaussian blur kernel size
+                 # Range of std deviation of the Gaussian blur kernel size (default (0.1, 5))
                  blur_kernel_std=(0.1, 5),
-                 # Transform brightness of image randomly from 1-brightness_factor to 1+brightness_factor
+                 # Transform brightness of image randomly from 1-brightness_factor to 1+brightness_factor (default 0.5)
                  brightness_factor=0.5
     ) -> None:
         super().__init__(root,
@@ -32,12 +32,8 @@ class AugmentedFlickrDataset(Flickr30k):
                          transform=transforms.Compose([
                              # Convert to tensor
                              transforms.ToTensor(),
-                             # Downsample to 224x224
-                             transforms.Resize([224, 224]),
-                             # Rotate from 0-360 deg, translate up to 1/5 of the image
-                             transforms.RandomAffine(degrees=360, translate=(0.2, 0.2)),
-                             # Blur with kernel a x b where a,b mean=5, std dev is uniform on [0.1, 5]
-                             transforms.GaussianBlur(kernel_size=(5,5), sigma=(0.1, 5)),
-                             # Change brightness from 0.5 to 1.5
-                             transforms.ColorJitter(brightness=0.5)
+                             transforms.Resize(resize),
+                             transforms.RandomAffine(degrees=degrees, translate=translate),
+                             transforms.GaussianBlur(kernel_size=blur_kernel_mean, sigma=blur_kernel_std),
+                             transforms.ColorJitter(brightness=brightness_factor)
                              ]))
