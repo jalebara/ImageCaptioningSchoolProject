b99d4b71199b17b63d77d6b1ca0aeed0b662b71c
Joshua Kalyanapu <jkalyan@ncsu.edu>
2022-02-12 05:18:04 +0000

    Implemented all required online augmentation of images, took samples
    * samples/0_downsample_rotate_translate_blur_brightness2.png:

--- src/data/augmentation.py
+++ src/data/augmentation.py
@@ -9,5 +9,35 @@ from torchvision.datasets import Flickr30k
 import torchvision.transforms as transforms
 
 class AugmentedFlickrDataset(Flickr30k):
-    def __init__(self, root="../../flickr30k", annotations_file="../../flickr30k/results_20130124.token") -> None:
-        super().__init__(root, annotations_file, transform=transforms.Resize([224, 224]))
+    def __init__(self,
+                 # Root directory of images
+                 root="../../flickr30k",
+                 # Location of annotations file
+                 annotations_file="../../flickr30k/results_20130124.token",
+                 # Tuple that is resize height and width
+                 resize=[224, 224],
+                 # Tuple that is range of degrees the image should be randomly rotated
+                 degrees=[0, 360],
+                 # Tuple that is range for random translation of image
+                 translate=[0.2, 0.2],
+                 # Mean values of the Gaussian blur kernel size
+                 blur_kernel_mean=(5, 5),
+                 # Range of std deviation of the Gaussian blur kernel size
+                 blur_kernel_std=(0.1, 5),
+                 # Transform brightness of image randomly from 1-brightness_factor to 1+brightness_factor
+                 brightness_factor=0.5
+    ) -> None:
+        super().__init__(root,
+                         annotations_file,
+                         transform=transforms.Compose([
+                             # Convert to tensor
+                             transforms.ToTensor(),
+                             # Downsample to 224x224
+                             transforms.Resize([224, 224]),
+                             # Rotate from 0-360 deg, translate up to 1/5 of the image
+                             transforms.RandomAffine(degrees=360, translate=(0.2, 0.2)),
+                             # Blur with kernel a x b where a,b mean=5, std dev is uniform on [0.1, 5]
+                             transforms.GaussianBlur(kernel_size=(5,5), sigma=(0.1, 5)),
+                             # Change brightness from 0.5 to 1.5
+                             transforms.ColorJitter(brightness=0.5)
+                             ]))
